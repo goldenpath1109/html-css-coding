4a461af57f61ae3d4785f95830a7bc706b5e1057
satodaiki <brilliantsunlight1109@gmail.com>
2024-03-19 00:22:27 +0900

    sp completed

---
+++ assets/js/modal.js
@@ -0,0 +1,21 @@
+//Get the modal
+var modal = document.getElementById("imgModal1");
+
+//Get the image
+var img = document.getElementById("photo1");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  modalImg.alt = this.alt;
+  captionText.style.display = "block";
+};
+
+var span = document.getElementsByClassName("close")[0];
+
+//click on (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
